bb687d6eea910031435c9b789bb5fff30b599102
Wilson E. Husin <wilsonehusin@gmail.com>
2017-02-07 22:40:13 -0500

    Basic server runs.

--- start.js
+++ start.js
@@ -1,27 +1,47 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var colors = require('colors');
+var port = 8081;
+var notfound = fs.readFileSync("./public/html/notfound.html");
 
 // Create a server
 http.createServer( function (request, response) {  
    // Parse the request containing file name
    var pathname = url.parse(request.url).pathname;
    
+   // Configuration for server navigation
+   var prepath = "./public";
+
    // Print the name of the file for which request is made.
    console.log("Request for " + pathname + " received.");
    
+   // Check if file format specified
+   if (!pathname.includes(".")) {
+      if (pathname.endsWith("/"))
+         pathname += "index.html";
+      else
+         pathname += "/index.html";
+   }
+
+   // Pre-path
+   if (pathname.endsWith(".ico"))
+      prepath += "/icons";
+   else
+      prepath += "/html";
+
    // Read the requested file content from file system
-   fs.readFile("./public/html" + pathname, function (err, data) {
+   fs.readFile(prepath + pathname, function (err, data) {
       if (err) {
-         console.log(err);
+         console.error("[" + "404".red + "] " + pathname + " not found");
          // HTTP Status: 404 [NOT FOUND]
          // Content Type: text/plain
          response.writeHead(404, {'Content-Type': 'text/html'});
 
          // Write the 404 template
-         response.write(fs.readFile("./public/notfound.html").toString());
+         response.write(notfound.toString());
       } else {
-         console.log("Access to " + pathname + " granted.");
+         console.log("[" + "200".green + "]" + " Access to " + pathname + " granted.");
          // HTTP Status: 200 [OK]
          // Content Type: text/plain
          response.writeHead(200, {'Content-Type': 'text/html'});	
@@ -32,7 +52,7 @@ http.createServer( function (request, response) {
       // Send the response body 
       response.end();
    });   
-}).listen(8081);
+}).listen(port);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at localhost:' + port);
