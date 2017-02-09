6baf68685f5771a0bba9b13863acc5be1d7e4b7f
Wilson E. Husin <wilsonehusin@gmail.com>
2017-02-08 20:05:02 -0500

    Restore npm start to start.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var hand = require('express-handlebars');
 var colors = require('colors');
 var prepath = __dirname + "/public";
 var host = "127.0.0.1";
@@ -8,10 +9,29 @@ var port = 5000;
 app.set('port', (process.env.PORT || port));
 app.use(express.static(prepath));
 
+app.set('views', prepath + "/html/pages");
+app.engine('handlebars', hand({
+    layoutsDir: prepath + "/html/pages/",
+    defaultLayout: "dashboard"
+}));
+app.set('view engine', 'handlebars');
+app.use('/vendor', express.static(prepath + "/html/vendor"));
+
 app.get('/', function(request, response) {
 	console.log("200".green + " requested page (/) granted.");
-	response.sendFile(prepath + "/html/pages/dashboard.html");
-})
+	response.status(200);
+    response.render('dashboard');
+});
+
+// Add new pages here
+
+
+// If nothing matches, go 404
+app.get('*', function(request, response) {
+    console.log("404".red + " requested page not found.");
+    response.status(404);
+    response.sendFile(prepath + "/html/notfound.html");
+});
 
 app.listen(app.get('port'), function() {
     console.log("LISTENING".magenta + " on port " + app.get('port'));
