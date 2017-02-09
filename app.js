fbf2ef4053a10d85cab10f5651a7afca5cbb8267
Wilson E. Husin <wilsonehusin@gmail.com>
2017-02-08 19:21:25 -0500

    Changed express settings.

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var app = express();
 var colors = require('colors');
 var prepath = __dirname + "/public";
 var host = "127.0.0.1";
-var port = 8080;
+var port = 5000;
+
+app.set('port', (process.env.PORT || port));
+app.use(express.static(prepath));
 
 app.get('/', function(request, response) {
 	console.log("200".green + " requested page (/) granted.");
 	response.sendFile(prepath + "/html/index.html");
 })
 
-var server = app.listen(port, host);
-console.log("LISTENING".green + " https://%s:%s", host, port);
+app.listen(app.get('port'), function() {
+    console.log("LISTENING".magenta + " on port " + app.get('port'));
+});
