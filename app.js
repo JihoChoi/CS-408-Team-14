52ef6ef790f00fb8c4e8cde3cb988d2c6e1ffada
Wilson E. Husin <whusin@purdue.edu>
2017-02-09 20:07:08 -0500

    Added stormpath configurations.

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
+// Dependencies
 var express = require('express');
 var app = express();
-var hand = require('express-handlebars');
 var colors = require('colors');
+var stormpath = require('express-stormpath');
+
+// Variables
 var prepath = __dirname + "/public/html";
 var host = "127.0.0.1";
 var port = 5000;
 
+// Set stuff for basic configurations
 app.set('port', (process.env.PORT || port));
 app.use(express.static(prepath));
-
 app.set('views', prepath + "/pages");
-
 app.use('/vendor', express.static(prepath + "/vendor"));
+
+// Stormpath stuff
+app.use(stormpath.init(app, {
+    client: {
+        apiKey: {
+            file: __dirname + "/config/stormpath/apikey.properties"
+        }
+    },
+    application: {
+        href: "https://api.stormpath.com/v1/applications/4sasSt9CuuGAi6LjYR5KZQ"
+    },
+    website: true
+}));
+
 
 app.get('/', function(req, res) {
 	console.log("200".green + " requested page (/) granted.");
@@ -34,6 +50,8 @@ app.get('*', function(req, res) {
     res.sendFile(prepath + "/notfound.html");
 });
 
-app.listen(app.get('port'), function() {
-    console.log("LISTENING".magenta + " on port " + app.get('port'));
+app.on('stormpath.ready', function() {
+    app.listen(app.get('port'), function() {
+        console.log("LISTENING".magenta + " on port " + app.get('port'));
+    });
 });
