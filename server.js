d6e0bd668e3d207ef20beddc87f2a4ca59835c9c
Wilson E. Husin <wilsonehusin@gmail.com>
2017-03-25 16:52:51 -0400

    What I have so far

--- server.js
+++ server.js
@@ -90,21 +90,23 @@ function courseVerify(req, res, next) {
     }
     // Check if course exists
     db.getAllCourses(function(courses) {
-        if (courses.indexOf(course) == -1) {            
-            res.status(404);
-            res.render('notfound', { url: req.url, layout: false });
-            console.log('404'.red + ' ' + req.user.emails[0].value + ' requested ' + req.url);
-            return;
-        }
-        // Check if enrolled
+        console.log(courses);
+    });
+    if (db.courseExists(course)) {
         if (req.user.courses.indexOf(course) < 0) {
             req.session.attemptedURL = req.url;
             res.redirect('/notpermitted');
             return;
         } else {
             next();
+            return;
         }
-    });
+    } else {
+        res.status(404);
+        res.render('notfound', { url: req.url, layout: false });
+        console.log('404'.red + ' ' + req.user.emails[0].value + ' requested ' + req.url);
+        return;
+    }
 };
 
 // /course/[coursename]/[groupname]
@@ -251,10 +253,13 @@ app.get('/course/*/createEvent', loginVerify, courseVerify, function(req, res) {
 
 app.get('/course/*/*', loginVerify, courseVerify, groupVerify, function(req, res) {
     var course = req.url.substr(8);
+    console.log('course: ' + course);
     if (course.indexOf('/') != -1) {
         var subgroup = course.substr(course.indexOf('/'));
         course = course.substr(0, course.indexOf('/'));
         req.session.lastCourse = course;
+        console.log('subgroup: ' + subgroup);
+        console.log('user: ' + req.user.emails[0].value);
         db.getGroup(subgroup, function(subgroup) {
             res.status(200);
             res.render('subgroup', {
@@ -263,11 +268,11 @@ app.get('/course/*/*', loginVerify, courseVerify, groupVerify, function(req, res
             });
             console.log('200'.green+ ' ' + req.user.emails[0].value + ' requested ' + req.url);
         });
-    } else {
-        res.status(404);
-        res.render('notfound', { url: req.url, layout: false });
-        console.log('404'.red + ' ' + req.user.emails[0].value + ' requested ' + req.url);
+        return;
     }
+    res.status(404);
+    res.render('notfound', { url: req.url, layout: false });
+    console.log('404'.red + ' ' + req.user.emails[0].value + ' requested ' + req.url);
 });
 
 app.get('/course/*', loginVerify, courseVerify, function(req, res) {
@@ -292,7 +297,7 @@ app.get('/course/*', loginVerify, courseVerify, function(req, res) {
     }
 });
 
-app.get('/chat/*', loginVerify, function(req, res) {
+app.get('/chat*', loginVerify, function(req, res) {
 	var url = req.url.substr(6);
 	var newurl = url.replace(/[^0-9a-zA-z]/gi, '');
 	if (newurl != url) {
