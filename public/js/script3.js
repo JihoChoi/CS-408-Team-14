5cf87c3314887f2c81cbf4903d185c95b458db94
Ic Tine <SignInSon@pal-nat186-83-44.itap.purdue.edu>
2017-03-29 23:03:11 -0400

    rsvp

---
+++ public/js/script3.js
@@ -0,0 +1,30 @@
+console.log("========================");
+console.log("INSIDE script3.js");
+console.log("========================");
+
+
+
+function submit_create_rsvp() {
+
+    console.log("========================");
+    console.log("Inside submit_create_rsvp()");
+    console.log("========================");
+    //var eventDate = "7/111";
+    var invite = document.forms["create-rsvp-form"]["invite"].value;
+    // var eventHour = document.forms["create-event-form"]["eventHour"].value;
+
+
+
+    alert("Invite : " + invite + "\n" +
+        "Event Created Successfully...");
+
+    // check if it already exist
+
+    //TODO
+
+
+    document.getElementById("create-rsvp-form").submit();
+
+
+
+}
